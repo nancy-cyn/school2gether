80ccafdba0d8f7bf4ff8dabdf10733b2c857cd99
Ntuthuko-Sandile Kubheka <ntuthukokubheka9@gmail.com>
2025-07-14 18:43:05 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Keep this if App.css has global styles, otherwise you might not need it.
+import Home from './Home'; // Import the Home component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Home /> /* Render the Home component */
+  )
 }
 
 export default App;
